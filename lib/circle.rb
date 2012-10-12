ea6563f8b8217d7156c204b56e8220711576ad0a
Adonai <adonai@localhost.localdomain>
2012-10-12 13:20:21 +0100

    'get_radius' instance method is changed to class method

--- lib/circle.rb
+++ lib/circle.rb
@@ -12,7 +12,7 @@ class Circle
 	@@Pi = 3.14
 
 	# method 'get_radius' returns the radius calculated from the diameter received
-	def get_radius(perimeter)
+	def self.get_radius(perimeter)
 		raise unless perimeter.is_a?(Numeric) && perimeter >= 0
 		perimeter / @@Pi / 2
 	end
